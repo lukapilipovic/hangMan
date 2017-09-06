00bbf780cddf5aee8860aa41e5f01600585cb21e
Luka Pilipovic <lukapilipovic@codija.lan>
2017-09-06 17:02:29 +0200

    b0.1

---
+++ js/game.js
@@ -0,0 +1,30 @@
+
+function loadGame () { 
+  // create a new div element 
+  // and give it some content 
+  let inputField = document.createElement("input");
+
+  let button = document.createElement("button");
+ 
+
+  button.id = "button";
+
+  document.body.appendChild(inputField);
+  document.body.appendChild(button);
+   
+  button.innerHTML = "Try letter";
+
+  
+}	
+	
+function setListener (elem){
+  this.
+}
+
+
+function checkEntry(letter){
+     
+	console.log("test");
+	if (letter != "")
+		alert (letter);
+} 
