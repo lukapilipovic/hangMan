436c9a49210fe5685d07131b64351eee4a422f01
Luka Pilipovic <lukapilipovic@codija.lan>
2017-09-07 11:53:40 +0200

    radi bez grafike

--- js/Hangman.js
+++ js/Hangman.js
@@ -1,46 +1,107 @@
 class Hangman {
 
     
-   
-
-    constructor(playerName,playerScore){
-      this.playerName = playerName;
-      this.playerScore = playerScore; 
+   static get BAR() {
+        return 7;
     }
 
-  
+    constructor(playerName,negativeScore,guess){
 
+      this.MAX_GUESS_NUM = Hangman.BAR;
+      this.playerName = playerName;
+      this.negativeScore = negativeScore; 
+      this.guess = guess;
+      this.numGuessed = 0;
+
+    }
+
+    
+
+
+    
     loadGame() { 
-    // create a new div element 
-    // and give it some content 
-
-
+    
+    
+     var fragment = document.createDocumentFragment();
 
 
      let inputField = document.createElement("input");
+     inputField.id = "inputField";
+
      let button = document.createElement("button");
      button.id = "button";
+
+     
+
+    
      button.innerHTML = "Try letter";
 
+     /* ne radi iznutra...ZASTO??
      button.onclick = function(){
          
-         this.checkEntry(inputField.value, "testrec");
+         checkEntry(inputField.value);
       };
+    */
+      
 
 
     document.body.appendChild(inputField);
     document.body.appendChild(button);
+    
+
+    let result = document.createElement("div");
+    result.className = "result";
+    
+
+    let counter  = 1;
+     for (let letter of this.guess)  {
+              
+              let letterBox = document.createElement("div");
+              letterBox.id = "letter"+counter;
+              
+              letterBox.className = "letters";
+              result.appendChild(letterBox);
+              counter++;  
+     }
+
+    document.body.appendChild(result);
 
     }	
 	
 
-    checkEntry(letter,word){
-     console.log(letter);
+    checkEntry(char) {
+        let counter = 1;
+        if (this.guess.indexOf(char) > -1){
+            for (let letter of this.guess)  {
+
+                if (char == letter){
+               
+                document.getElementById('letter' + counter).innerHTML = letter;
+                this.numGuessed++
+                if (this.numGuessed == this.guess.length)
+                    {   
+                        console.log("You won");
+                    }
+
+                }
 
 
+                counter++;
+            }
+        }
+        else {
+                this.negativeScore++;
+                console.log("You missed! Only " + (this.MAX_GUESS_NUM-this.negativeScore) + " tries left!");
 
+            }
 
-    } 
+            console.log(hangman);
+
+    }
+
+    
+
+    
 
 
 }
